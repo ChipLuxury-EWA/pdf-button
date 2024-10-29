f464a50c42a02ab80264e93af96b0f73d4ae215c
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-10-29 08:53:13 +0200

    added an amazing reset button

--- src/_components/SignatureBox.jsx
+++ src/_components/SignatureBox.jsx
@@ -4,22 +4,10 @@ const githubButtonStyle = {
   display: "inline-block",
   outline: 0,
   cursor: "pointer",
-  padding: "1px 4px",
+  padding: 0,
+  margin: 0,
   fontSize: 12,
-  fontWeight: 500,
   lineHeight: "15px",
-  verticalAlign: "middle",
-  border: "1px solid",
-  borderRadius: 6,
-  color: "#ffffff",
-  backgroundColor: "#2ea44f",
-  borderColor: "#1b1f2326",
-  boxShadow: "rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset",
-  transition: "0.2s cubic-bezier(0.3, 0, 0.5, 1)",
-  transitionProperty: "color, background-color, border-color",
-  "&:hover": {
-    backgroundColor: "darkgreen",
-  },
 };
 
 const SignatureBox = ({ width = 250, height = 100, positionX = 10, positionY = 110, sigRef, buttonPosition }) => {
@@ -36,7 +24,7 @@ const SignatureBox = ({ width = 250, height = 100, positionX = 10, positionY = 1
         style={{
           width,
           height,
-          border: "1px solid #000",
+          border: "1px solid rgba(0,0,0,0.1)",
           cursor: "crosshair",
           backgroundColor: "#fff",
         }}
@@ -51,7 +39,7 @@ const SignatureBox = ({ width = 250, height = 100, positionX = 10, positionY = 1
         />
       </div>
       <button style={githubButtonStyle} onClick={clear}>
-        reset
+      🔄
       </button>
     </div>
   );
