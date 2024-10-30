3ac708613d7a72006c3569c98b47912ccccad209
UriKimhi <uri.kimhi@gmail.com>
2024-10-30 09:35:29 +0200

    push to tom check

--- src/RPDF.jsx
+++ src/RPDF.jsx
@@ -1,38 +1,78 @@
-import { Document, Page, pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
 import pdfFile from "./assets/tofes-161.pdf";
-import { useEffect, useState } from "react";
-import { useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import SignatureBox from "./_components/SignatureBox";
 import { PDFDocument } from "pdf-lib";
+import * as pdfjsLib from "pdfjs-dist/build/pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
+// Set pdfjs worker
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
+function createBlock(className) {
+  const block = document.createElement("div"); // Create a <div> element
+  block.className = className; // Set the class name
+  return block; // Return the created element
+}
+async function findSignaturePosition(pdfUrl, searchText, searchText2) {
+  const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
+  const signatureLocations = [];
+
+  for (let pageIndex = 0; pageIndex < pdf.numPages; pageIndex++) {
+    const page = await pdf.getPage(pageIndex + 1);
+    const textContent = await page.getTextContent();
+
+    for (const item of textContent.items) {
+      if (item.str.includes(searchText) || item.str.includes(searchText2)) {
+        console.log(item);
+        const viewport = page.getViewport({ scale: 1 });
+        console.log(viewport);
+        const transform = item.transform; // Transformation matrix
+
+        // Calculate the coordinates of the text in PDF
+        const x = transform[4];
+        const y = viewport.height - transform[5];
+
+        console.log("y: " + y);
+        console.log("x:" + x);
+
+        const adjustedX = x;
+        const adjustedY = y;
+
+        signatureLocations.push({
+          page: pageIndex + 1,
+          x: adjustedX,
+          y: adjustedY,
+          width: item.width, // Adjust width as per requirements
+          height: 43,
+        });
+      }
+    }
+  }
+
+  console.log("Signature locations:", signatureLocations.length > 0 ? signatureLocations : "No signatures found");
+  return signatureLocations;
+}
 
 const RPDF = () => {
   const [numPages, setNumPages] = useState(null);
+  const [signatureLocation, setSignatureLocation] = useState([]);
   const [modifiedPdfUrl, setModifiedPdfUrl] = useState(null);
-  const [signatures, setSignatures] = useState([
-    {
-      shouldRender: true,
-      ref: useRef(null),
-      sigDrawn: false,
-      page: 3,
-      sigPosition: { x: 173, y: 640, width: 180, height: 58 },
-    },
-    {
-      shouldRender: true,
-      ref: useRef(null),
-      sigDrawn: false,
-      page: 4,
-      sigPosition: { x: 74, y: 81, width: 180, height: 58 },
-    },
-    {
-      shouldRender: true,
-      ref: useRef(null),
-      sigDrawn: false,
-      page: 4,
-      sigPosition: { x: 297, y: 81, width: 180, height: 58 },
-    },
-  ]);
+  const [signatures, setSignatures] = useState([]);
+
+  useEffect(() => {
+    const findPosition = async () => {
+      const location = await findSignaturePosition(pdfFile, "חתימה", "חותמת");
+      const newSignatures = location.map((el) => ({
+        shouldRender: true,
+        // ref: useRef(null),
+        sigDrawn: false,
+        page: el.page,
+        sigPosition: { x: el.x, y: el.y, width: 180, height: 58 },
+      }));
+      setSignatures(newSignatures);
+    };
+
+    findPosition();
+  }, []);
 
   const allSignaturesDrawn = signatures.some((sig) => !sig.sigDrawn);
 
@@ -52,7 +92,6 @@ const RPDF = () => {
     const existingPdfBytes = await fetch(pdfFile).then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
-    // Embed signatures
     const pngImagePromises = signatureCanvases.map((canvas) => fetch(canvas.toDataURL()).then((res) => res.arrayBuffer()));
     const pngImageBytes = await Promise.all(pngImagePromises);
     const pngImages = await Promise.all(pngImageBytes.map((bytes) => pdfDoc.embedPng(bytes)));
@@ -65,9 +104,7 @@ const RPDF = () => {
 
     const modifiedPdfBytes = await pdfDoc.save();
     const blob = new Blob([modifiedPdfBytes], { type: "application/pdf" });
-    const url = URL.createObjectURL(blob);
-
-    setModifiedPdfUrl(url);
+    setModifiedPdfUrl(URL.createObjectURL(blob));
   };
 
   useEffect(() => {
